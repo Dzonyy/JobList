80f275b5816872d45840995411d26e37534ce636
Dzonyy <paszcza.jakub@gmail.com>
2019-11-13 21:31:46 +0100

    added admin controllers

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,4 @@
+module Admin
+  class JobsController < ::Admin::ApplicationController
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  
+  has_one_attached :avatar
+  has_one_attached :image
+
   validates :email, presence: true, uniqueness: true
 end
