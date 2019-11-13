3022c1ec68ce012313775a1a260617b8ad0a269b
Dzonyy <paszcza.jakub@gmail.com>
2019-11-13 19:52:46 +0100

    added Employees model and controller, added Admin:SessionsController

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_204519) do
+ActiveRecord::Schema.define(version: 2019_11_13_184656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_employees_on_email", unique: true
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "name"
