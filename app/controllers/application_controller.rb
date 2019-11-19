0305e9b04b82ed819905afa6054ec67a0fbe206d
Dzonyy <paszcza.jakub@gmail.com>
2019-11-19 09:45:46 +0100

    fixed admibn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,22 @@ class ApplicationController < ActionController::Base
   #     'application'
   #   end
   # end
+  def query_param?
+    params[:s].present?
+  end
 
+  def query_param
+    "/%#{params[:s]}%"
+  end
+
+  def after_sign_up_path_for(resource)
+    my_account_path
+  end
+
+  def back_path(default = '/')
+    request.headers["X-XHR-Referer"] || request.referer || default
+  end
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
