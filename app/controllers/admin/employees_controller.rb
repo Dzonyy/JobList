cf32a6ddde9ce48c1ae5b0a55de835382b1e7aca
Dzonyy <paszcza.jakub@gmail.com>
2019-11-20 21:07:17 +0100

    devise erb views converted to haml

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,10 +1,32 @@
 module Admin
   class EmployeesController < ResourceController
+    load_and_authorize_resource
 
     private
 
     def permitted_params
-      %i[:email :password]
+      %i[
+        email first_name last_name role
+        password password_confirmation phone_number
+      ]
+    end
+
+    # cleaning empty password attributes
+    def resource_params
+      object_params = super
+
+      if object_params[:password].blank?
+        object_params.delete(:password)
+        if object_params[:password_confirmation].blank?
+          object_params.delete(:password_confirmation)
+
+          if action_name == 'create'
+            object_params[:password] = Devise.friendly_token[0, 20]
+            object_params[:password_confirmation] = object_params[:password]
+          end
+        end
+      end
+      object_params
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'customers/registrations' }, skip: :sessions
+  devise_for :users, controllers: { registrations: 'users/registrations' }, skip: :sessions
   devise_for :employees, only: %i[sessions passwords]
+
+  devise_scope :user do
+    get 'logowanie', to: 'devise/sessions#new', as: :new_user_session
+    post 'logowanie', to: 'devise/sessions#create', as: :user_session
+    delete 'wyloguj', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    # get 'rejestracja', to: 'devise/registrations#new', as: :new_user_registration
+    # post 'rejestracja', to: 'devise/registrations#create', as: :user_registration
+  end
+
   root 'home#index'
 
   namespace :admin do
