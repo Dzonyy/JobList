af01fcf08fc3d7a752cc1285317b46ea7d781477
Dzonyy <paszcza.jakub@gmail.com>
2020-01-26 11:18:17 +0100

    added benefits mode, controller, form and list

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
     resources :users, except: :show
     resources :technologies, except: :show
     resources :categories, except: :show
+    resources :benefits, except: :show
   end
 end
