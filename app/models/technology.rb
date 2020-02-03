da297755f354865d08adcc8e3a2ac1e3f708019d
Jakub Paszcza <paszcza.jakub@gmail.com>
2020-02-03 16:44:11 +0100

    updated job partial, added validations, fixed pricing

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -3,4 +3,7 @@ class Technology < ApplicationRecord
   has_many :job, through: :job_technologies
 
   has_one_attached :icon
+
+  validates :name, presence: true
+  # validates :color, presence: true
 end
