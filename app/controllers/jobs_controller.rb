59d8925d1cfbc1635f6a15e855d1b7d6ce1caf44
Dzonyy <paszcza.jakub@gmail.com>
2020-02-17 19:21:08 +0100

    fixed forms

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,6 +8,12 @@ class JobsController < ApplicationController
   end
 
   def create
+    @job = Job.new(params[:job_params])
+    if @job.save
+      redirect_to @job
+    else
+      render "new"
+    end
   end
 
   def job_modal
@@ -22,7 +28,8 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(
-      %i[name]
+      %i[name category_id pricing_id salary_from salary_to experience company_name company_city address slug] +
+      [job_technologies: %i[name rating]]
     )
   end
 end
